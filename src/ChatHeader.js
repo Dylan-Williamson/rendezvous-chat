3c881ef4b1082dec791e3a2904df7674eefdb226
github username <dirtyassweed@icloud.com>
2021-03-14 12:23:25 -0400

    adds chat header

---
+++ src/ChatHeader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './ChatHeader.css';
+import { Notifications, EditLocationRounded, PeopleAltRounded, HelpRounded, SearchRounded, SendRounded } from '@material-ui/icons';
+
+const ChatHeader = () => {
+    return (
+        <div className="chatHeader">
+            <h3>header</h3>
+            <div className="chatHeader__left">
+                <h3>
+                    <span className="chatHeader__hash">
+                        #
+                    </span>
+                    Test Channel Name
+                </h3>
+            </div>
+            <div className="chatHeader__right">
+                <Notifications />
+                <EditLocationRounded />
+                <PeopleAltRounded />
+                <div className="chatHeader__search">
+                    <input placeHolder="Search" />
+                    <SearchRounded />
+                </div>
+                <SendRounded />
+                <HelpRounded />
+            </div>
+        </div>
+    )
+}
+
+export default ChatHeader
